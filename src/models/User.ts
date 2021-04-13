1d25618465728eada144db4b45b6154e5c9fbb40
Augusto Lima <augustolimads@gmail.com>
2021-04-12 21:29:13 -0300

    refactor user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,23 +2,16 @@ import knex from "../database/connection";
 import bcrypt from "bcrypt";
 
 class User {
-
   async new(email: string, password: string, name: string) {
-
     try {
       const hash = await bcrypt.hash(password, 10);
 
       await knex
         .insert({ email, password: hash, name, role: 0 })
         .table("users");
-
-
     } catch (error) {
-        
       console.error(error);
-
     }
-
   }
 
   async findUserByEmail(email: string) {
@@ -26,7 +19,6 @@ class User {
       const result = await knex.select("*").from("users").where({ email });
 
       return result.length > 0 ? true : false;
-        
     } catch (error) {
       console.error(error);
       return false;
